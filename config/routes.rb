fb2bcc4b9c32cdedb48980cfc5fb08257cc28a32
Luis Felipe <actuario.felipe@gmail.com>
2017-03-23 22:39:24 -0600

    migración y form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'pages/x'
 
+  post 'pages/save_user'
+
   root 'pages#x'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
